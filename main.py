3299de79e5933d82835ba03afea2898dedb4b115
captain-spicy <103981886+captain-spicy@users.noreply.github.com>
2022-04-28 05:47:17 -0500

    Initial version of password generator program. Finished adding in the create and verify password section. Will work on adding dictionary with sites next, then making a password generator for those sites.

---
+++ main.py
@@ -0,0 +1,43 @@
+class PasswordGenerator:
+    def __init__(self, sites):
+        self._set_password = None       # sets initial password to null
+        self._sites = list(sites)       # creates an empty list to hold sites
+        self._show_password = False     # sets initial password visibility to none
+
+    def push(self, urls):               # creates function to insert new sites into list
+        self._sites.insert(0, urls)
+
+    def set_password(self):             # creates account password
+        self._set_password = input("Please create an account password: ")
+        return self._set_password       # returns the new password to replace null
+
+    @property
+    def show_password(self):
+        return self._show_password      # currently show password is set to False at SoS
+
+    @show_password.setter
+    def show_password(self, value):     # requests account password, giving 3 chances before locking up
+        counter = 0
+
+        while counter < 3:
+
+            if value:
+                password = input("Please enter your password: ")
+                if password == self._set_password:
+                    print("Correct password entered!")
+                    self._show_password = value
+                    break
+                else:
+                    counter += 1
+                    print("Incorrect password! Try again!")
+
+        if counter == 3:
+            raise ValueError("Too many guesses! Account locked!")
+
+
+new_account = PasswordGenerator(["google", "yahoo", "facebook"])
+new_account.set_password()
+new_account.show_password = True
+
+
+
